d478909a37600e1de3277f9d113df124abc9ec37
PegahMazhin <nirvana.pva@gmail.com>
2024-11-06 09:31:35 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
